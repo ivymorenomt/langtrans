9b834324bcbce5c396b5b7a9da5690c4f49823a0
Marjorie Ivy <56984774+ivymorenomt@users.noreply.github.com>
2025-01-31 21:19:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.title("Translator")
 source_text = st.text_area("Enter text to translate")
 if source_text:
     detected_language_code = translator.detect(source_text).lang
-    detected_language_name = language_dict[detected_language_code, "Unknown"]
-    st.write(f"Detected language: {detected_language_name.capitalize()}")
+    detected_language_name = language_dict.get(detected_language_code, "Unknown")
+    st.write(f"Detected language: {detected_language_name}")
 
 dest_lang = st.selectbox("Select a language", languages)
 if st.button("Translate") and source_text:
